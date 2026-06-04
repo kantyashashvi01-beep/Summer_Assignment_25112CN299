1aa92d8667bfd58df93eb7eb33c60cbd6e189389
kantyashashvi01-beep <kantyashi01@gmail.com>
2026-06-04 21:03:30 +0530

    Add Fibonacci sequence calculation
    Implement Fibonacci sequence generator

---
+++ day-4/ques1.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+ int main(){
+     int x,a=0,b=1,next;
+      printf("the number:");
+       scanf("%d",&x);
+        for(int i=0;i<x;i++){ 
+            if(i==0){ 
+                next=a;
+             }
+             else if(i==1){
+                 next=b; 
+            }else{
+              next=a+b; 
+              a=b;
+              b=next;
+         } 
+             printf("%d ",next); 
+          } 
+         return 0; 
+            }
