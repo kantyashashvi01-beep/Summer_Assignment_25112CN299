4e0dddd23c23fc54fafa6c42319a1191f35e14a5
kantyashashvi01-beep <kantyashi01@gmail.com>
2026-06-04 21:04:17 +0530

    Update ques2.c

---
+++ day-4/ques2.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+int fibonacci(int n) {
+    if (n < 0) {
+        return -1; 
+    }
+    if (n == 0 || n == 1) {
+        return n;
+    }
+
+    int a = 0; 
+    int b = 1; 
+    int c;     
+
+    for (int i = 2; i <= n; i++) {
+        c = a + b;
+        a = b;
+        b = c;
+    }
+
+    return b;
+}
+
