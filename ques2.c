95b4ee411d9d2fa77c7f6d258e1ec6940c3323d4
kantyashashvi01-beep <kantyashi01@gmail.com>
2026-06-05 19:23:35 +0530

    Add files via upload

--- ques2.c
+++ ques2.c
@@ -1,37 +1,26 @@
-#include <stdio.h>
-int fibonacci(int n) {
-    if (n < 0) {
-        return -1; 
+#include<stdio.h>
+int main(){
+    int n,p=1,sum=0,r,a;
+    printf("the number:");
+    scanf("%d",&n);
+    a=n;
+    for(int i=1;n!=0;i++){
+        r=n%10;
+        for(int j=1;j<=r;j++){
+            p=p*j;
+        }
+        sum=sum+p;
+        p=1;
+        n=n/10;
+
     }
-    if (n == 0 || n == 1) {
-        return n;
+    if(sum==a){
+        printf("%d is a strong number",a);
+
     }
-
-    int a = 0; 
-    int b = 1; 
-    int c;     
-
-    for (int i = 2; i <= n; i++) {
-        c = a + b;
-        a = b;
-        b = c;
-    }
-
-    return b;
-}
-
-int main() {
-    int n;
-    printf("Enter the value of n: ");
-    scanf("%d", &n);
-
-    int result = fibonacci(n);
-    if (result == -1) {
-        printf("Fibonacci is not defined for negative numbers.\n");
-    } else {
-        printf("The %d-th Fibonacci number is: %d\n", n, result);
+    else{
+        printf("%d is not a strong number",a);
     }
     
     return 0;
-
 }
