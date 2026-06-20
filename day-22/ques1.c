e4f6c3d2823a276d3f08c62b5c4055cd76de9fa8
Utkarsh <utkarshkhampar@gmail.com>
2026-06-20 20:30:55 +0530

    day-22 assignment uploaded

---
+++ day-22/ques1.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+int main() {
+    char str[100];
+    int i, length = 0, flag = 1;
+
+    printf("Enter a string: ");
+    gets(str);
+
+    // Find length of string
+    while(str[length] != '\0') {
+        length++;
+    }
+
+    // Check palindrome
+    for(i = 0; i < length / 2; i++) {
+        if(str[i] != str[length - i - 1]) {
+            flag = 0;
+            break;
+        }
+    }
+
+    if(flag)
+        printf("String is Palindrome");
+    else
+        printf("String is Not Palindrome");
+
+    return 0;
+}
